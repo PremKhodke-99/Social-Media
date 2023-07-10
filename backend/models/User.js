258eda37b87ad11595edf5a1f52f35a03d71fd71
PremKhodke-99 <premkhodke.99@gmail.com>
2023-07-10 16:18:33 +0530

    first commit

---
+++ backend/models/User.js
@@ -0,0 +1,59 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const userSchema = new mongoose.Schema({
+
+    name: {
+        type: String,
+        required: [true, "Please enter a name"],
+    },
+
+    avatr: {
+        public_id: String,
+        url: String,
+    },
+
+    email: {
+        type: String,
+        required: [true, "Please enter a email"],
+        unique: [true, "Email already exists"],
+    },
+
+    password: {
+        type: String,
+        required: [true, "Please enter a password"],
+        minlength: [6, "Password must be at least 6 characters"],
+        select: false,
+    },
+
+    posts: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Post",
+    }],
+
+    followers: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "user",
+        }
+    ],
+
+    following: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "user",
+        }
+    ],
+
+});
+
+
+userSchema.pre("save", async function(next){
+    if(this.isModified("password")){
+        this.password = await bcrypt.hash(this.password, 10);
+    }
+
+    next();
+});
+
+module.exports = mongoose.model("User", userSchema);
