e32f8d0620d526d08c2f94c0ad5a3ddb7a6c613d
PremKhodke-99 <premkhodke.99@gmail.com>
2023-07-10 17:04:42 +0530

    second commit

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -19,12 +19,53 @@ exports.register = async (req, res) => {
 
         user = await User.create({ name, email, password, avatar: { public_id: "sample_id", url: "sampleurl" }, });
 
-        res.status(201).json({success:true, user});
+        res.status(201).json({ success: true, user });
 
     } catch (error) {
         res.status(500).json({
             success: false,
             message: error.message,
         });
+    }
+}
+
+exports.login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email }).select("+password");
+
+        if (!user) {
+            return res.status(400).json({
+                success: false,
+                message: "User does not exist"
+            });
+        }
+        const isMatch = await user.matchPassword(password);
+
+        if (!isMatch) {
+            return res.status(400).json({
+                success: false,
+                message: "Incorrect password",
+            });
+        }
+
+        const token = await user.generateToken();
+
+        const options = {
+            expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
+            httpOnly: true,
+        };
+
+        res.status(200).cookie("token", token, options).json({
+            success: true,
+            user,
+            token,
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message,
+        })
     }
 }
